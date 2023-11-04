b503a8b8b1f7b052364e7561db968b1ab0390127
Mike Cellini <mike.cellini@protonmail.com>
2023-11-04 00:55:37 -0500

    Setup lsp

--- .config/nvim/lua/anhyzer/lazy-bootstrap.lua
+++ .config/nvim/lua/anhyzer/lazy-bootstrap.lua
@@ -13,13 +13,28 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.4',
-	dependencies = { 'nvim-lua/plenary.nvim' }
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.4',
+        dependencies = { 'nvim-lua/plenary.nvim' }
     },
     {
-	'nvim-treesitter/nvim-treesitter',
-	build = ":TSUpdate"
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate"
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
+
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+        },
     },
     'shaunsingh/nord.nvim',
     'lambdalisue/nerdfont.vim',
